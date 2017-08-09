7f405fdb88ebd0507064f6ed4fbc80a7f73d1304
nigamshah29 <nigam129@gmail.com>
2017-08-09 11:40:17 -0500

    smart contract compiles

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,26 +13,26 @@ App = {
   web3Provider: null,
   contracts: {},
 
-  init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
+  // init: function() {
+  //   // Load pets.
+  //   $.getJSON('../pets.json', function(data) {
+  //     var petsRow = $('#petsRow');
+  //     var petTemplate = $('#petTemplate');
 
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
+  //     for (i = 0; i < data.length; i ++) {
+  //       petTemplate.find('.panel-title').text(data[i].name);
+  //       petTemplate.find('img').attr('src', data[i].picture);
+  //       petTemplate.find('.pet-breed').text(data[i].breed);
+  //       petTemplate.find('.pet-age').text(data[i].age);
+  //       petTemplate.find('.pet-location').text(data[i].location);
+  //       petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
 
-        petsRow.append(petTemplate.html());
-      }
-    });
+  //       petsRow.append(petTemplate.html());
+  //     }
+  //   });
 
-    return App.initWeb3();
-  },
+  //   return App.initWeb3();
+  // },
 
   initWeb3: function() {
     // Initialize web3 and set the provider to the testRPC.
@@ -48,15 +48,15 @@ App = {
   },
 
   initContract: function() {
-    $.getJSON('Adoption.json', function(data) {
+    $.getJSON('Coder.json', function(data) {
       // Get the necessary contract artifact file and instantiate it with truffle-contract.
-      var AdoptionArtifact = data;
-      App.contracts.Adoption = TruffleContract(AdoptionArtifact);
+      var CoderArtifact = data;
+      App.contracts.Coder = TruffleContract(CoderArtifact);
 
       // Set the provider for our contract.
-      App.contracts.Adoption.setProvider(App.web3Provider);
+      App.contracts.Coder.setProvider(App.web3Provider);
 
-      // Use our contract to retieve and mark the adopted pets.
+      // Use our contract to retrieve and mark the adopted pets.
       return App.markAdopted();
     });
     return App.bindEvents();
